53b9c2b93586eb4d22726518191bad1e25c381b2
Miguel Angel <miguelapb35@gmail.com>
2020-10-15 18:45:01 -0500

    Updates

---
+++ precache-manifest.41e878b5f5032e668a537053126a9f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b58730a7e0b6c15d78663cce712d80b",
+    "url": "/react-pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "1fdc5962bfc261d66d27",
+    "url": "/react-pixabay-image-finder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b060ffcb8f392d0840a3",
+    "url": "/react-pixabay-image-finder/static/js/2.0f358868.chunk.js"
+  },
+  {
+    "revision": "ab790d7f4927e929140961c444db2a72",
+    "url": "/react-pixabay-image-finder/static/js/2.0f358868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdc5962bfc261d66d27",
+    "url": "/react-pixabay-image-finder/static/js/main.2d9122f2.chunk.js"
+  },
+  {
+    "revision": "35af632b5cb60137e3ff",
+    "url": "/react-pixabay-image-finder/static/js/runtime-main.a7071fb4.js"
+  }
+]);
